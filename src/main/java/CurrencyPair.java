cffc0c563871e04d3267963ff22a56cd51203749
adrianrogalski <adrianrogalski99@gmail.com>
2021-07-18 20:15:29 +0200

    Dodana podstawowa logika obsługi kwot oraz metody ich kupna i sprzedazy do klasy CurrencyPair

--- src/main/java/CurrencyPair.java
+++ src/main/java/CurrencyPair.java
@@ -1,2 +1,37 @@
+import java.math.BigDecimal;
+
 public class CurrencyPair {
+    private BigDecimal amount;
+    private BigDecimal spread;
+    private BigDecimal conversionFactor;
+
+    public CurrencyPair(double spread, double conversionFactor) {
+        // spread jest wartoscia procentowa
+        // conversion factor jest liczba
+        this.spread = BigDecimal.valueOf(spread);
+        this.conversionFactor = BigDecimal.valueOf(conversionFactor);
+    }
+
+    public double sell(double inputAmount) {
+        BigDecimal amount = BigDecimal.valueOf(inputAmount);
+        BigDecimal sellValue = amount.multiply(conversionFactor).subtract(spread.divide(BigDecimal.valueOf(100)).multiply(amount));
+        return sellValue.doubleValue();
+    }
+
+    public double buy(double inputAmount) {
+        BigDecimal amount = BigDecimal.valueOf(inputAmount);
+        BigDecimal purchaseValue = amount.divide(conversionFactor).add(spread.divide(BigDecimal.valueOf(100)).multiply(amount));
+        return purchaseValue.doubleValue();
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "CurrencyPair{" +
+                "amount=" + amount +
+                ", spread(%)=" + spread +
+                ", conversionFactor=" + conversionFactor +
+                '}';
+    }
 }
--- src/main/java/ExchangeApp.java
+++ src/main/java/ExchangeApp.java
@@ -1,5 +1,10 @@
+import java.math.BigDecimal;
+
 public class ExchangeApp {
     public static void main(String[] args) {
-
+        CurrencyPair plnEur = new CurrencyPair(6,0.25);
+        double kwota = plnEur.buy(101);
+        plnEur.sell(100);
+        System.out.println(kwota);
     }
 }
